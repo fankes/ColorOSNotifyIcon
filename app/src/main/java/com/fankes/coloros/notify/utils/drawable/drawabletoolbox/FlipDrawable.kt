b762e9f2706080602c97a4d4e9169b10ee556aeb
fankesyooni <qzmmcn@163.com>
2022-06-10 17:25:08 +0800

    Update Gradle & Kotlin & PlatformSDK - Update Kotlin version to 1.7.0 - Update Gradle dependencies - Merge legacy code

--- app/src/main/java/com/fankes/coloros/notify/utils/drawable/drawabletoolbox/FlipDrawable.kt
+++ app/src/main/java/com/fankes/coloros/notify/utils/drawable/drawabletoolbox/FlipDrawable.kt
@@ -20,7 +20,7 @@
  *
  * This file is Created by fankes on 2022/1/8.
  */
-@file:Suppress("DEPRECATION", "CanvasSize")
+@file:Suppress("DEPRECATION", "CanvasSize", "OVERRIDE_DEPRECATION")
 
 package com.fankes.coloros.notify.utils.drawable.drawabletoolbox
 
