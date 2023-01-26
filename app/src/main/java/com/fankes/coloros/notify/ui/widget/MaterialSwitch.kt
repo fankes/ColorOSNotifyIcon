ba83aa28f7e8aab1a39cd6e22fcd7035123d04b1
fankesyooni <qzmmcn@163.com>
2023-01-27 00:09:15 +0800

    Modify rename ui/view to ui/widget

--- app/src/main/java/com/fankes/coloros/notify/ui/widget/MaterialSwitch.kt
+++ app/src/main/java/com/fankes/coloros/notify/ui/widget/MaterialSwitch.kt
@@ -22,7 +22,7 @@
  */
 @file:Suppress("SameParameterValue")
 
-package com.fankes.coloros.notify.ui.view
+package com.fankes.coloros.notify.ui.widget
 
 import android.content.Context
 import android.content.res.ColorStateList
