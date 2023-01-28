1f5680194959fb43f4d29e6b48d5cd12a8241017
fankesyooni <qzmmcn@163.com>
2023-01-29 00:45:35 +0800

    Modify move new app support functions to a new way to implement and shielded some apps that shouldn't appear in SystemUIHooker

--- app/src/main/java/com/fankes/coloros/notify/hook/entity/SystemUIHooker.kt
+++ app/src/main/java/com/fankes/coloros/notify/hook/entity/SystemUIHooker.kt
@@ -25,8 +25,10 @@
 package com.fankes.coloros.notify.hook.entity
 
 import android.app.WallpaperManager
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.content.res.ColorStateList
 import android.graphics.Bitmap
 import android.graphics.Color
@@ -94,9 +96,6 @@ object SystemUIHooker : YukiBaseHooker() {
     private const val IconManagerClass = "$SYSTEMUI_PACKAGE_NAME.statusbar.notification.icon.IconManager"
 
     /** 原生存在的类 */
-    private const val PluginManagerImplClass = "$SYSTEMUI_PACKAGE_NAME.shared.plugins.PluginManagerImpl"
-
-    /** 原生存在的类 */
     private const val NotificationBackgroundViewClass = "$SYSTEMUI_PACKAGE_NAME.statusbar.notification.row.NotificationBackgroundView"
 
     /** 原生存在的类 */
@@ -156,12 +155,6 @@ object SystemUIHooker : YukiBaseHooker() {
     private val OplusPowerNotificationWarningsClass = VariousClass(
         "com.oplusos.systemui.notification.power.OplusPowerNotificationWarnings",
         "com.coloros.systemui.notification.power.ColorosPowerNotificationWarnings"
-    )
-
-    /** 根据多个版本存在不同的包名相同的类 */
-    private val AbstractReceiverClass = VariousClass(
-        "com.oplusos.systemui.common.receiver.AbstractReceiver",
-        "com.coloros.systemui.common.receiver.AbstractReceiver"
     )
 
     /** 根据多个版本存在不同的包名相同的类 */
@@ -542,8 +535,43 @@ object SystemUIHooker : YukiBaseHooker() {
 
     /** 注册生命周期 */
     private fun registerLifecycle() {
-        /** 解锁后重新刷新状态栏图标防止系统重新设置它 */
-        onAppLifecycle { registerReceiver(Intent.ACTION_USER_PRESENT) { _, _ -> if (isUsingCachingMethod) refreshStatusBarIcons() } }
+        onAppLifecycle {
+            /** 解锁后重新刷新状态栏图标防止系统重新设置它 */
+            registerReceiver(Intent.ACTION_USER_PRESENT) { _, _ -> if (isUsingCachingMethod) refreshStatusBarIcons() }
+            /** 注册定时监听 */
+            registerReceiver(Intent.ACTION_TIME_TICK) { context, _ ->
+                if (isEnableHookColorNotifyIcon() && prefs.get(DataConst.ENABLE_NOTIFY_ICON_FIX_AUTO))
+                    IconAdaptationTool.prepareAutoUpdateIconRule(context, prefs.get(DataConst.NOTIFY_ICON_FIX_AUTO_TIME))
+            }
+            onCreate {
+                /** 注册发送适配新的 APP 图标通知监听 */
+                registerReceiver(object : BroadcastReceiver() {
+                    override fun onReceive(context: Context, intent: Intent?) {
+                        if (isEnableHookColorNotifyIcon()) intent?.also {
+                            if (it.action.equals(Intent.ACTION_PACKAGE_REPLACED).not() &&
+                                it.getBooleanExtra(Intent.EXTRA_REPLACING, false)
+                            ) return@also
+                            it.data?.schemeSpecificPart?.also { packageName ->
+                                when (it.action) {
+                                    Intent.ACTION_PACKAGE_ADDED -> {
+                                        if (iconDatas.takeIf { e -> e.isNotEmpty() }
+                                                ?.filter { e -> e.packageName == packageName }
+                                                .isNullOrEmpty()
+                                        ) IconAdaptationTool.pushNewAppSupportNotify(context, packageName)
+                                    }
+                                    Intent.ACTION_PACKAGE_REMOVED -> IconAdaptationTool.removeNewAppSupportNotify(context, packageName)
+                                }
+                            }
+                        }
+                    }
+                }, IntentFilter().apply {
+                    addDataScheme("package")
+                    addAction(Intent.ACTION_PACKAGE_ADDED)
+                    addAction(Intent.ACTION_PACKAGE_REPLACED)
+                    addAction(Intent.ACTION_PACKAGE_REMOVED)
+                })
+            }
+        }
         /** 刷新图标缓存 */
         SystemUITool.Host.onRefreshSystemUI(param = this) { recachingPrefs(it) }
     }
@@ -832,55 +860,6 @@ object SystemUIHooker : YukiBaseHooker() {
                                     }
                                 }
                         }
-                }
-            }
-        }
-        /** 发送适配新的 APP 图标通知 */
-        PluginManagerImplClass.hook {
-            injectMember {
-                method {
-                    name = "onReceive"
-                    param(ContextClass, IntentClass)
-                }
-                afterHook {
-                    if (isEnableHookColorNotifyIcon()) args().last().cast<Intent>()?.also {
-                        if (it.action.equals(Intent.ACTION_PACKAGE_REPLACED).not() &&
-                            it.getBooleanExtra(Intent.EXTRA_REPLACING, false)
-                        ) return@also
-                        when (it.action) {
-                            Intent.ACTION_PACKAGE_ADDED ->
-                                it.data?.schemeSpecificPart?.also { newPkgName ->
-                                    if (iconDatas.takeIf { e -> e.isNotEmpty() }
-                                            ?.filter { e -> e.packageName == newPkgName }
-                                            .isNullOrEmpty()
-                                    ) IconAdaptationTool.pushNewAppSupportNotify(args().first().cast()!!, newPkgName)
-                                }
-                            Intent.ACTION_PACKAGE_REMOVED ->
-                                IconAdaptationTool.removeNewAppSupportNotify(
-                                    context = args().first().cast()!!,
-                                    packageName = it.data?.schemeSpecificPart ?: ""
-                                )
-                        }
-                    }
-                }
-            }
-        }
-        /** 自动检查通知图标优化更新的注入监听 */
-        AbstractReceiverClass.hook {
-            injectMember {
-                method {
-                    name = "onReceive"
-                    param(ContextClass, IntentClass)
-                }
-                afterHook {
-                    args().first().cast<Context>()?.also {
-                        /** 注册定时监听 */
-                        if (isEnableHookColorNotifyIcon() && prefs.get(DataConst.ENABLE_NOTIFY_ICON_FIX_AUTO))
-                            IconAdaptationTool.prepareAutoUpdateIconRule(
-                                context = it,
-                                timeSet = prefs.get(DataConst.NOTIFY_ICON_FIX_AUTO_TIME)
-                            )
-                    }
                 }
             }
         }
